d02da56a5bc679693e309b02158a7eaeda04d36c
Ayush <kayushtiwari26@gmail.com>
2023-08-15 20:02:10 +0530

    Mobile Compatible

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import "./Nav.css";
-import logo from "../../assests/logo.jpg"
+import React, { useState } from 'react';
+import './Nav.css';
+import logo from '../../assests/logo.png';
 
-const nav = () => {
+
+const Nav = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       window.scrollTo({
         top: section.offsetTop,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
+      closeMenu(); // Close the menu after clicking on a link
     }
   };
 
   return (
-    <div className='NavbarBg'>
-      <div className='Navhead'>
-        <img src={logo} alt='logo' className='NavImg' />
-        <div className='Navleftdiv'>
-          <ul className='Navlist'>
+    <div className="NavbarBg">
+      <div className="Navhead">
+        <img src={logo} alt="logo" className="NavImg" />
+        <div className="Navleftdiv">
+          <ul className="Navlist">
             <li onClick={() => scrollToSection('home')}>Home</li>
             <li onClick={() => scrollToSection('about')}>About</li>
             <li onClick={() => scrollToSection('education')}>Education</li>
@@ -26,9 +38,23 @@ const nav = () => {
             <li onClick={() => scrollToSection('contact')}>Contact</li>
           </ul>
         </div>
+        <div className={`NavMenuIcon ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      <div className={`NavOverlay ${isMenuOpen ? 'active' : ''}`} onClick={closeMenu}>
+        <ul className="Navlist OverlayNavList">
+          <li onClick={() => scrollToSection('home')}>Home</li>
+          <li onClick={() => scrollToSection('about')}>About</li>
+          <li onClick={() => scrollToSection('education')}>Education</li>
+          <li onClick={() => scrollToSection('portfolio')}>Portfolio</li>
+          <li onClick={() => scrollToSection('contact')}>Contact</li>
+        </ul>
       </div>
     </div>
   );
 };
 
-export default nav;
+export default Nav;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -6,7 +6,7 @@ const Section = () => {
   
   const downloadCV = () => {
     // Replace this URL with the actual path to your PDF file
-    const pdfUrl = process.env.PUBLIC_URL + '/AyushLatestLogoResume.pdf';
+    const pdfUrl = process.env.PUBLIC_URL + '/AyushLatestPhotoResume.pdf';
     
     // Create an anchor element
     const anchor = document.createElement('a');
@@ -23,7 +23,7 @@ const Section = () => {
           <div className="Sectionleft" id="home">
               <div className='Paraleft'>
               <div className='Line1'><p> <span1 style={{color: "#FD4955"}}>Ayush Tiwari</span1></p></div>
-              <div className='Line2'><p>I'm a<span style={{color: "#FD4955", fontSize:"29px"}}> </span></p></div>
+              <div className='Line2'><p>I'm a<span style={{color: "#FD4955"}}> </span></p></div>
               <div className='Line3'><p>"I love designing websites and am looking for more opportunities to work in coding on a freelance basis."</p></div>
              <div className='aButt'><button class="button-77" role="button" onClick={downloadCV} >Download CV</button></div>
 
